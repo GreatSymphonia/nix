4b396a1ffc4f41a40f808bce6e7d11623d63fdcc
Louis Raymond <louis.raymond2017@gmail.com>
2026-04-25 13:54:12 -0400

    override gtkrc

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,7 @@ in {
       ./home
     ];
   };
+  home-manager.backupFileExtension = "backup";
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.extraSpecialArgs = { inherit unstable; };
