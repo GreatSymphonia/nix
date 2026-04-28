9b362d2bf3a4b97c5ec1aa178afed858ad1dc0d5
Louis Raymond <louis.raymond2017@gmail.com>
2026-04-28 16:44:26 -0400

    wip

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -11,20 +11,18 @@
     package = pkgs.wireshark;
   };
 
-  {
-    programs.obs-studio = {
-      enable = true;
+  programs.obs-studio = {
+    enable = true;
 
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-        obs-backgroundremoval
-        obs-pipewire-audio-capture
-        obs-vaapi #optional AMD hardware acceleration
-        obs-gstreamer
-        obs-vkcapture
-      ];
-    };
-  }
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi #optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
 
   nixpkgs.config.allowUnfree = true;
 
