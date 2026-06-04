55e2b5eb84c257f4652a7452559ce94930cc5d54
Louis Raymond <louis.raymond2017@gmail.com>
2026-06-04 14:26:43 -0400

    wip

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -6,7 +6,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot = {
-    kernelPackages = pkgs.linuxPackages_6_6;
     extraModulePackages = [ config.boot.kernelPackages.evdi ];
     initrd = {
       # Modules that are always loaded by the initrd.
